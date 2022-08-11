37f37c85f16a3dd7e6dc81fde79a0736426329f2
ric <ricardo.monteverde@codeandtheory.com>
2022-08-11 13:43:06 +0800

    feat: New project with deployment target of 14.0

---
+++ SwiftUIJewelry/SwiftUIJewelryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIJewelryApp.swift
+//  SwiftUIJewelry
+//
+//  Created by Ricardo Monteverde on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIJewelryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
